c89b556188955d5720732e76a15eafc5c6f155c8
Bit-Jaeger <147092976+Bit-Jaeger@users.noreply.github.com>
2026-04-04 18:10:40 +0200

    adding DOMContentLoaded Event

--- scripts_main.js
+++ scripts_main.js
@@ -1,19 +1,21 @@
 
 
+document.addEventListener('DOMContentLoaded', (event) => {
+        
+    let dailyScores = 0
+    let totalVisitors = 0
+    document.querySelector('#add1').onclick = function(){
+        //adding the buttonclick
+        dailyScores++;
+        totalVisitors++;
 
-let dailyScores = 0
-let totalVisitors = 0
-document.querySelector('#add1').onclick = function(){
-    //adding the buttonclick
-    dailyScores++;
-    totalVisitors++;
+        document.querySelector('#dailyVisitors').textContent = dailyScores;
 
-    document.querySelector('#dailyVisitors').textContent = dailyScores;
+        document.querySelector('#totalVisitors').textContent = totalVisitors;
 
-    document.querySelector('#totalVisitors').textContent = totalVisitors;
+        document.querySelector('#add1_Feedback').textContent = "Thanks for participating!";
+    }
 
-    document.querySelector('#add1_Feedback').textContent = "Thanks for participating!";
+
+    document.querySelector('#dailyVisitors')
 }
-
-
-document.querySelector('#dailyVisitors')
