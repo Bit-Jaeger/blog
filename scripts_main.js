7b2d23f2c7124f568835936406ab7593d4df94e2
Bit-Jaeger <147092976+Bit-Jaeger@users.noreply.github.com>
2026-04-04 18:00:56 +0200

    fixing table

--- scripts_main.js
+++ scripts_main.js
@@ -4,8 +4,14 @@
 let dailyVisitors = 0
 let totalVisitors = 0
 document.querySelector('#add1').onclick = function(){
+
     dailyVisitors++;
-    document.querySelector('#dailyVisitors').innerHTML = dailyVisitors
-    document.querySelector('#add1_Feedback').innerHTML = "Thanks for participating!"
+    totalVisitors++;
+
+    document.querySelector('#dailyVisitors').innerHTML = dailyVisitors;
+    document.querySelector('#totalVisitors').innerHTML = totalVisitors;
+    document.querySelector('#add1_Feedback').innerHTML = "Thanks for participating!";
 }
+
+
 document.querySelector('#dailyVisitors')
