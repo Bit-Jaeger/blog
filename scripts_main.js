3ff29f6a83ec376d900fecc2af74de7890133fbb
Bit-Jaeger <147092976+Bit-Jaeger@users.noreply.github.com>
2026-04-04 17:46:43 +0200

    fixing interactive table

--- scripts_main.js
+++ scripts_main.js
@@ -4,8 +4,8 @@
 let dailyVisitors = 0
 let totalVisitors = 0
 document.querySelector('#add1').onclick = function(){
-    dailyVisitors = dailyVisitors + 1;
-    document.querySelector('dailyVisitors').innerHTML = dailyVisitors
-    document.querySelector('#add1_feedback').innerHTML = "Thanks for participating!"
+    dailyVisitors++;
+    document.querySelector('#dailyVisitors').innerHTML = dailyVisitors
+    document.querySelector('#add1_Feedback').innerHTML = "Thanks for participating!"
 }
 document.querySelector('#dailyVisitors')
