221baba206a2576113b0a64daeb3d6c28844b590
Johannes Riedel <>
2026-04-08 11:54:13 +0200

    adding button to change themes

--- scripts_main.js
+++ scripts_main.js
@@ -1,3 +1,7 @@
+const themes = ["styles_main.css", "styles_main_retro.css"];
+let theme_count = 0;
+
+
 
 
 document.addEventListener('DOMContentLoaded', (event) => {
@@ -20,3 +24,14 @@ document.addEventListener('DOMContentLoaded', (event) => {
 )
 
 
+document.addEventListener('DOMContentLoaded', (event) => {
+    //changing the font on button change_themes click
+    document.querySelector('#change_theme').onclick = function()
+        {
+            const styles_variable = themes[theme_count % themes.length];
+            theme_count++;
+            document.getElementById('styles').setAttribute('href', styles_variable);
+            console.log("theme variable changed.")
+        }
+}
+)
