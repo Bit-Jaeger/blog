5884ce30067f2d3f289d5874522754e09283e109
Bit-Jaeger <147092976+Bit-Jaeger@users.noreply.github.com>
2026-04-22 17:50:18 +0200

    fixing jekyll build error

--- assets/js/scripts_main.js
+++ assets/js/scripts_main.js
@@ -11,5 +11,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
             document.getElementById('styles').setAttribute('href', styles_variable);
             console.log("theme variable changed.")
         }
+    
 }
 )
